30b72375175e58f4b9fd8f7d61ecc7cfe9431fb0
LudFav <45117462+LudFav@users.noreply.github.com>
2025-10-21 11:56:57 +0200

    Update SequentialScanner.jsx

--- src/components/ui/SequentialScanner.jsx
+++ src/components/ui/SequentialScanner.jsx
@@ -45,18 +45,30 @@ const SequentialScanner = () => {
     );
   };
   const currentTextRef = useRef(TEXT_BASE);
-useEffect(() => {
+  useEffect(() => {
     const tl = gsap.timeline({ repeat: -1, defaults: { ease: "none" } });
     const targetBar = barRef.current;
     gsap.set(targetBar, { opacity: 1 });
     const toggleBar = (startTime, duration) => {
-        tl.call(() => {
-            targetBar.classList.add('is-animated');
-        }, null, startTime);
-        tl.call(() => {
-            targetBar.classList.remove('is-animated');
-        }, null, startTime + duration);
-        tl.to(targetBar, { opacity: 0.1, duration: 0.05, yoyo: true, repeat: 1 }, startTime);
+      tl.call(
+        () => {
+          targetBar.classList.add("is-animated");
+        },
+        null,
+        startTime
+      );
+      tl.call(
+        () => {
+          targetBar.classList.remove("is-animated");
+        },
+        null,
+        startTime + duration
+      );
+      tl.to(
+        targetBar,
+        { opacity: 0.1, duration: 0.05, yoyo: true, repeat: 1 },
+        startTime
+      );
     };
 
     toggleBar(0.7, 0.3);
@@ -66,10 +78,20 @@ useEffect(() => {
     toggleBar(4.7, 0.3);
     tl.add(scramble(TEXT_ALT_ONE, "alt-one", 0.3), 4.7);
     toggleBar(6.7, 0.3);
-    tl.add(scramble(TEXT_BASE, "base", 0.3), 6.7);    
+    tl.add(scramble(TEXT_BASE, "base", 0.3), 6.7);
     return () => {
       tl.kill();
     };
-}, []);
+  }, []);
+
+  return (
+    <div ref={containerRef} className="sequential-scanner">
+      <div ref={barRef} className="scanner-bar"></div>
+      <div ref={textRef} className={`scanner-text ${colorClass}`}>
+        {displayText}
+      </div>
+    </div>
+  );
+};
 
 export default SequentialScanner;
